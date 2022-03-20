c8131d435a9f3e8f655a7e60b9eaa1139894f5dc
Nickea <nickeabennett@gmail.com>
2022-03-20 18:39:00 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+const message = [];
+function writeCards(name) {
+  for (let c = 0; c < name.length; c++) {
+    message.push(`Thank you, ${name[c]}, for the wonderful surprise gift!`);
+  }
+  return message;
+}
+writeCards[message];
+
+function countDown(number) {
+    let count = 11;
+    while (count > 0) {
+        console.log(count-- -1);
+    }
+}
+console.log(count-- -1);
